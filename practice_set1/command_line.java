3baa8e367f9284449a77b46538d34008cf8ac067
Nihar420 <parwaninihar11@gmail.com>
2026-02-18 09:54:46 +0530

    Minnor Change

--- practice_set1/command_line.java
+++ practice_set1/command_line.java
@@ -14,7 +14,7 @@ public class command_line
 int args0 = Integer.parseInt(args[0]);
 int args1 = Integer.parseInt(args[1]);
 
-System.out.println("This is the command line program has argument:"+ args[0]+"and"+args[1]);
+System.out.println("This is the command line program has argument: "+ args[0]+" and "+args[1]);
 
 int sum = args0 + args1;
 System.out.println("The sum of the two arguments is: " + sum);
